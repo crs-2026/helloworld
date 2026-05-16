edd29811cadd0f12759349e141875c2ef1b42860
crs-2026 <galsec37@gmail.com>
2026-05-16 18:19:13 +0200

    Update calc_test.py

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -63,7 +63,7 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.substract, "0", 0)
 
     def test_divide_method_fails_with_zero_division(self):
-        self.assertRaises(ZeroDivisionError, self.calc.divide, 2, 0)
+        self.assertRaises(TypeError, self.calc.divide, 2, 0)
         
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
